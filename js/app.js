98c2876171b7f1e84d6c60655d7190a284f1b7a9
Adam <adamlapacz@gmail.com>
2021-05-20 00:52:42 +0200

    add content, change in nav: mobile: width, background, galssmorphism

--- js/app.js
+++ js/app.js
@@ -1,29 +1,31 @@
 //AdLap 2021
 
-const menuAnimation = document.querySelector('#burger');
+const burger = document.querySelector('#burger');
 const menu = document.querySelector('#menu');
 const media = window.matchMedia('(min-width: 750px)');
 
-menuAnimation.addEventListener('click', function () {
-    menuAnimation.classList.toggle('menu-visible');
+burger.addEventListener('click', function () {
+    burger.classList.toggle('menu-visible');
 
-    if (menuAnimation.classList.contains('menu-visible')) {
-        menu.style.transform = 'translateY(70px)'
+    if (burger.classList.contains('menu-visible')) {
+        menu.style.transform = 'translateY(50px)'
     }
 
-    if (!menuAnimation.classList.contains('menu-visible')) {
+    if (!burger.classList.contains('menu-visible')) {
         menu.style.transform = 'translateY(-200%)';
     }
 });
 
 menu.addEventListener('click', function () {
-    menu.style.transform = 'translateY(-200%)';
-    menuAnimation.classList.remove('menu-visible');
+    // menu.style.transform = 'translateY(-200%)';
+    media ? menu.style.transform = 'translateY(0)' : menu.style.transform = 'translateY(-200%)';
+    // burger.classList.remove('menu-visible');
+
 });
 
 media.addEventListener('change', function (e) {
     if (e.matches) {
-        menuAnimation.classList.remove('menu-visible');
+        burger.classList.remove('menu-visible');
         menu.style.transform = 'translateY(0)';
     }
 
